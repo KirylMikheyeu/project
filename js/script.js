a20e60157c5d0103ce40b9d03c3080f4270b46e2
Kiryll <mamontiy@gmail.com>
2021-09-16 20:16:17 +0300

    first part

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+    count: numberofFilms,
+    muvies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Сколько фильмов вы уже посмотрели?',''),
+      d =  prompt('На сколько оцените его?','');
+
+personalMovieDB.muvies[a] = b; 
+personalMovieDB.muvies[c] = d;           
+
+console.log(personalMovieDB);      
